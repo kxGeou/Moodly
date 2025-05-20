7751eb0d3d70a73b57e24a66e7834887b3691a12
kxGeou <grzegorzm.work@gmail.com>
2025-05-20 21:28:40 +0200

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,18 +13,18 @@ export default function Hero() {
     }
   }
     return (
-        <section className="flex flex-col justify-between relative w-full px-2 gap-8"> 
-            <div className="flex flex-col gap-4 items-center text-center">
-                <h2 className="text-[2rem] font-bold text-brown leading-[120%] ">Twój Personalny Dziennik Nastroju</h2>
-                <p className="text-lg font-light text-light-brown leading-[130%] ">Moddly pomoże ci podsumować twoje samopoczucie oraz oczyścić głowe z przemyśleń za pomocą codziennej ankiety oraz notatki</p>
-                <button onClick={handleRegister} className="bg-brown mt-2 rounded-xl px-2 py-2 w-[75%] text-white cursor-pointer">Sprawdź Moodly</button>
+        <section className="flex flex-col items-center justify-between relative w-full px-2 gap-8 lg:flex-row lg:items-center lg:justify-around lg:h-[70vh] lg:gap-0"> 
+            <div className="flex flex-col gap-4 items-center text-center lg:max-w-[40%] lg:items-start">
+                <h2 className="text-[2rem] font-bold text-brown leading-[120%] lg:text-[3rem] lg:text-left">Twój Personalny Dziennik Nastroju</h2>
+                <p className="text-lg font-light text-light-brown leading-[130%] lg:text-xl lg:text-left lg:leading-[140%]">Moddly pomoże ci podsumować twoje samopoczucie oraz oczyścić głowe z przemyśleń za pomocą codziennej ankiety oraz notatki</p>
+                <button onClick={handleRegister} className="bg-brown mt-12 rounded-xl px-2 py-3 w-full text-white cursor-pointer lg:mt-8 lg:w-[75%]">Sprawdź Moodly</button>
             </div>
 
             <div>
                 <ul>
                     {hero.map((h, index) => (
-                        <li key={index} className="flex flex-col justify-start items-center text-brown gap-6 text-lg">
-                        <span className="bg-green w-6 h-6 rounded-full flex justify-center items-center mt-8">
+                        <li key={index} className="flex flex-col justify-start items-center text-brown gap-6 text-lg lg:flex-row lg:mt-6">
+                        <span className="bg-green w-6 h-6 rounded-full flex justify-center items-center mt-8 lg:mt-0">
                             <i className="fa-solid fa-plus text-white text-lg"></i>
                         </span>
                             {h}
